00a1d9077ffaa526da84dde36e977890947f9375
Daniel Stiven Martinez <danimarcast@icloud.com>
2022-05-13 14:59:29 -0500

    Update Mezcla_Arroz.R

--- Mezclas_Gaussianas_aplicaciones/Mezcla_Arroz.R
+++ Mezclas_Gaussianas_aplicaciones/Mezcla_Arroz.R
@@ -1,50 +1,93 @@
+#Carga de  libreria mclust para poder correr un modelo de mezclas gaussinas
+#revisar Cran para leer la documentación detallada de todas lafunciones 
+#de la funcion mclust
+#descomentar y correr la linea siguiente en caso de que no se tenga instalado
+## la paqueteria mclust
+## install.packages("mclust")
+##  Cargar la libreria pROC, para poder hacer evaluación multiclases
+#usanddo curva y roc, ademas de calcular el área-
+## #descomentar y correr la linea siguiente en caso de que no se tenga instalado
+## la paqueteria pROC
+## install.packages("pROC")
 library(readxl)
 library(mclust)
 library(pROC)
+
+# Carga del Dataset "Rice_MSC_Dataset",cambiar la ruta de acuerdo al lugar 
+#en donde se encuentre alojado el archivo
 Rice_MSC_Dataset <- read_excel("Rice_MSC_Dataset/Rice_MSC_Dataset.xlsx")
+
+#Filrar los datos, de manera que solo tomemos las primeras 4 columnas y la ultima
+#columna
 
 ArrozDatos<-Rice_MSC_Dataset[1:75000,- (5:106)]
 
+#Fijamos una semilla para que obtengan los mismos resultados 
+#obtenidos en el archivo
 set.seed(2022)
+
+#Creamos una muestra de indices, que nos ayudaran a dividir los datos 
+#en datos  de prueba y datos de entrenamiento
 indices<-sample(1:nrow(ArrozDatos),size = floor(0.40*nrow(ArrozDatos)),replace = FALSE)
-entreno<-ArrozDatos[indices,-c(4:16,17)]
+#Corriendo este table puede observar como se estan tomando los datos 
+#de entrenamiento
 table(ArrozDatos[indices,]$CLASS)
+# Creamos el data frame X el cual solo contiene las caracteristicas 
+#para los datos de entrenamiento
 X<-ArrozDatos[indices,1:4]
-y <-ArrozDatos[indices,5]
 
+# Esta parte corresponen con el grafico de dispersion para los datos 
+#de entrenamiento, donde se puede observar una dispersion por pares
+#lo que permite identificar en cuantos grupos se estan agrupando los datos
 
-pairs(X,main="Diagrama de Dispersion",labels=c("Area","Perimetro","EjeMayor","EjeMenor"),lower.panel = NULL,col = as.factor(ArrozDatos[indices,]$CLASS))
-par(xpd=TRUE)
-legend(0,0.55,as.vector(unique(ArrozDatos[indices,]$CLASS)),bty='l')
+colores<-hcl.colors(5,"Plasma")
+vectores<-as.vector(unique(ArrozDatos[indices,]$CLASS))
+plot(X,main="Diagrama de Dispersion",lower.panel=NULL,col=colores[as.factor(ArrozDatos[indices,]$CLASS)],
+     labels=c("Area","Perimetro","EjeMayor","EjeMenor"),pch=19)
+par(xpd=T)
+legend(x=0,y=0.49,legend = c("Arborio","Basmati","Ipsala","Jasmine","Karacadag"),fill=colores,cex = 0.6)
+
+#Corremos la funcion Mclust, indicando "G", el numero de componentes
 
 GMM_Arroz<-Mclust(X,G=5)
-summary(GMM_Arroz,parameters = TRUE)
+#Esta linea siguiente la puede descomentar en caso de que desee 
+#observar los parametros de cada componente,
+#indicando parameters=TRUE puede observar todas los parametros de cada componente,en caso de no
+#hacerlos solo mostrara una información minima
+#summary(GMM_Arroz,parameters = TRUE)
+
+#Aqui puede ver la matriz de confusión para los datos de entrenamiento
 table(ArrozDatos[indices,]$CLASS,GMM_Arroz$classification)
 
+#con la función predict puede hacer las predicciones para los datos de prueba
+#teniendo en cuenta los indices que creamos 
+# puede probar inventando un dato con caracteristicas aleatoria
+#y ver a que componente pertenece
+
 prediccion<-predict(GMM_Arroz,newdata = ArrozDatos[-indices,1:4],type='response')
-table(ArrozDatos[-indices,]$CLASS,prediccion$classification)
+#Tabla para ver como se habian tomado los datos de prueba
 table(ArrozDatos[-indices,]$CLASS)
+#Matriz de confusión para los datos de prueba, cabe aclarar que no tiene
+#la forma directa de una matriz de confusión por el orden en como la funcion esta creando las componentes
+table(ArrozDatos[-indices,]$CLASS,prediccion$classification)
+
+
+#Usamos la funcion multiclass.roc para realizar evaluacion multiclase
+#dentro de variable creada podra encontrar las curvas rocs realizadas por pares
 
 x<-multiclass.roc(ArrozDatos[-indices,]$CLASS,prediccion$classification)
 x
 colores<-c("#FE12DA","#FF6202","#FE2312","#FFB202","#FFFF02","#C2FF02",
            "#28CF60","#28CDCF","#2860CF")
 rs <- x[['rocs']]
-plot.roc(rs[[1]],xlab = "Especificidad",ylab = "Sensitividad",legacy.axes = F,axes = T,xlim = c(1,0))
+#graficamos las curvas roc con sus respectivas AUC
+plot.roc(rs[[1]],xlab = "1-Especificidad",ylab = "Sensibilidad",legacy.axes = T,axes = T,print.thres = F,
+         grid = T,col=1)
+
 sapply(1:length(rs),function(i){ 
   if (i<=5){
-    plot.roc(rs[[i]],col=colores[i],add=T,print.auc=T,print.auc.x=0.23,print.auc.y=0.5-0.09*i,axes=T,legacy.axes=T)
+    plot.roc(rs[[i]],col=colores[i],add=T,print.auc=T,print.auc.x=0.23,print.auc.y=0.5-0.09*i,axes=T,legacy.axes=F,print.thres = F)
   }
     if(i>5){
-      plot.roc(rs[[i]],col=colores[i],add=T,print.auc=T,print.auc.x=0,print.auc.y=0.091*i-0.5,axes=T,legacy.axes=T)}
+      plot.roc(rs[[i]],col=colores[i],add=T,print.auc=T,print.auc.x=0,print.auc.y=0.091*i-0.5,axes=T,legacy.axes=T,print.thres = F)}
 })
-sensitividades<-c()
-especificidades<-c()
-for (i in 1:10){
-  sensitividades[i]<-mean(x$rocs[[i]]$sensitivities)
-  especificidades[i]<-mean(x$rocs[[i]]$specificities)
-}
-sensitividad<-mean(sensitividades)
-especificidad<-mean(especificidades)
-
-
